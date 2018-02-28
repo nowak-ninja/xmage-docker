67d40e04e00432490208aeab2ccd3dab80b11f0f
Sebastian Nowak <sebastian.nowak@hexagram.com>
2018-02-28 08:34:07 +0100

    removed my script

--- startServer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -Xms256M -Xmx512M -XX:MaxPermSize=256m -Djava.security.policy=./config/security.policy -Djava.util.logging.config.file=./config/logging.config -Dlog4j.configuration=file:./config/log4j.properties -jar ./lib/mage-server-1.4.26.jar
