856a00ec8b66910bb02b69efd7481ef842f65b16
Sebastian Nowak <nowak-ninja@users.noreply.github.com>
2020-04-05 00:50:07 +0200

    Shorten timeout

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # just in case stop running service
-docker-compose down
+docker-compose down -t 3
 
 # build image without cache to fetch always fresh server
 docker build -t xmage:latest --no-cache .
